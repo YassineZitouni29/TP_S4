7b918c785b4e02c809a3f51e71f0fd301bacd2b4
YassineZitouni29 <158048035+YassineZitouni29@users.noreply.github.com>
2025-02-09 12:41:34 +0100

    Update main.cpp

--- TP2_Yassine_ZITOUNI/main.cpp
+++ TP2_Yassine_ZITOUNI/main.cpp
@@ -5,16 +5,16 @@
 
 #if EXO == 1
 int main(){
-#define PI 3.14
-#ifdef DEBUGG
-    std::cout<<"It's defined";
+#define PI 3.14  // define PI
+#ifdef DEBUG  //check if the macro DEBUG is defined
+    std::cout<<"It's defined";  //if so i display this message
 #endif
 return 0;
 }
 #elif EXO == 2
-int main(int argc, char* argv[]){
-    for (int i=1; i<argc; ++i){
-        std::cout<<"Argument "<<i <<": "<<argv[i]<<std::endl;
+int main(int argc, char* argv[]){  
+    for (int i=1; i<argc; ++i){  // I iterate over the argument given by the user
+        std::cout<<"Argument "<<i <<": "<<argv[i]<<std::endl;  // I display each of them
     }
     return 0;
 }
@@ -22,17 +22,17 @@ int main(int argc, char* argv[]){
 int main(){
     using namespace std;
     int variable;
-    std::cin >> variable;
-    std::cout << "the variable is "<< variable<<std::endl;
-    cout<< "the variable is "<< variable<< std::endl;
+    std::cin >> variable;  // I take the variable given
+    std::cout << "the variable is "<< variable<<std::endl; //with std::cout
+    cout<< "the variable is "<< variable<< std::endl;  //without
     return 0;
 }
 #elif EXO == 4
 int main(){
-    int var1; 
-    float var2;
-    std::cin>> var1 >> var2;
-    std::cout << "the entered variables are " << var1 << " et " <<var2 <<std::endl;
+    int var1; // the first variable is an integer
+    float var2; // the second variable is a float
+    std::cin>> var1 >> var2;  // I take both of them
+    std::cout << "the entered variables are " << var1 << " et " <<var2 <<std::endl;  // I display
     return 0;
 }
 #endif
