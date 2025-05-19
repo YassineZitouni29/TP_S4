66970f60864af871d3fa257cabd99b3cbb4125cd
YassineZitouni29 <158048035+YassineZitouni29@users.noreply.github.com>
2025-05-19 07:24:46 +0100

    Update Cloud_util.hpp

--- MINIPROJECT1_YASSINEZITOUNI/Cloud_util.hpp
+++ MINIPROJECT1_YASSINEZITOUNI/Cloud_util.hpp
@@ -6,4 +6,6 @@
 using namespace std;
 void display(const KubernetesCluster& cluster);
 void deployPods(KubernetesCluster& cluster, std::vector<std::unique_ptr<Pod>>& pods);
+void saveClusterMetrics(const KubernetesCluster& cluser,const string& filename);
+void forEachContainer(const KubernetesCluster& cluster, const function<void(const Container&)>& func);
 #endif
